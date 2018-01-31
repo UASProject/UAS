c2f49b347046e9663947edd00dbd775ad6594f45
cjemery <cjemery90@Hotmail.com>
2018-01-31 13:55:27 -0600

    added updated files

--- UAS_Functions.py
+++ UAS_Functions.py
@@ -42,4 +42,64 @@ def servo_close():
 
 	GPIO.cleanup()
 
+def arm_and_takeoff(aTargetAltitude):
+    """Arms vehicle and fly to aTargetAltitude."""
+    
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+    
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+    print(" Waiting for arming...")
+    time.sleep(1)
+    
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+    
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+def goto(xdistance, ydistance, gotoFunction=vehicle.simple_goto):
+    """
+        Moves the vehicle to a position dNorth metres North and dEast metres East of the current position.
+        
+        The method takes a function pointer argument with a single `dronekit.lib.LocationGlobal` parameter for
+        the target position. This allows it to be called with different position-setting commands.
+        By default it uses the standard method: dronekit.lib.Vehicle.simple_goto().
+        
+        The method reports the distance to target every two seconds.
+        """
+    
+    currentLocation = vehicle.location.global_relative_frame
+    targetLocation = get_location_metres(currentLocation, dNorth, dEast)
+    targetDistance = get_distance_metres(currentLocation, targetLocation)
+    gotoFunction(center)
+    
+    #print "DEBUG: targetLocation: %s" % targetLocation
+    #print "DEBUG: targetLocation: %s" % targetDistance
+    
+    while vehicle.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
+        #print "DEBUG: mode: %s" % vehicle.mode.name
+        remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, center)
+        print("Distance to target: ", remainingDistance)
+        if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
+            print("Reached target")
+            break;
+        time.sleep(2)
+
+
 
