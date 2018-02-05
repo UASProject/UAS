78a993c6eb44724b43bfaca9adbf1fd02fac0d5a
cjemery <cjemery90@Hotmail.com>
2018-02-05 16:02:21 -0600

    update UAS files"

--- UAS_Functions.py
+++ UAS_Functions.py
@@ -15,11 +15,16 @@ def turnOffMotors():
 	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
 	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
+#################  Stepper Control ####################
+
 def lightbulb_detach():
 	myStepper=mh.getStepper(200,1)
 	myStepper.setSpeed(120)
 	for i in range(12):
 		myStepper.step(100, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
+
+#################  Servo control ####################
+
 
 def servo_close():
 	GPIO.setmode(GPIO.BOARD)
@@ -44,7 +49,11 @@ def servo_close():
 		time.sleep(.1)
 
 	GPIO.cleanup()
-'''
+
+
+#################  Takeoff Function ####################
+
+
 def arm_and_takeoff(aTargetAltitude):
     """Arms vehicle and fly to aTargetAltitude."""
     
@@ -71,40 +80,14 @@ def arm_and_takeoff(aTargetAltitude):
     #  after Vehicle.simple_takeoff will execute immediately).
     while True:
         print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+        #Trigger just below target alt.
             print("Reached target altitude")
             break
         time.sleep(1)
 
-def goto(xdistance, ydistance, gotoFunction=vehicle.simple_goto):
-    """
-        Moves the vehicle to a position dNorth metres North and dEast metres East of the current position.
-        
-        The method takes a function pointer argument with a single `dronekit.lib.LocationGlobal` parameter for
-        the target position. This allows it to be called with different position-setting commands.
-        By default it uses the standard method: dronekit.lib.Vehicle.simple_goto().
-        
-        The method reports the distance to target every two seconds.
-        """
-    
-    currentLocation = vehicle.location.global_relative_frame
-    targetLocation = get_location_metres(currentLocation, dNorth, dEast)
-    targetDistance = get_distance_metres(currentLocation, targetLocation)
-    gotoFunction(center)
-    
-    #print "DEBUG: targetLocation: %s" % targetLocation
-    #print "DEBUG: targetLocation: %s" % targetDistance
-    
-    while vehicle.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
-        #print "DEBUG: mode: %s" % vehicle.mode.name
-        remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, center)
-        print("Distance to target: ", remainingDistance)
-        if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
-            print("Reached target")
-            break;
-        time.sleep(2)
 
-'''
+#################  Pixy Control Function ####################
 
 def GetBlocks():
 
@@ -137,6 +120,8 @@ def GetBlocks():
 				#print(y)
 				time.sleep(1)
 				return x,y;
+
+#################  Navigation Function ####################
 
 def Centering():
 	while True:
--- UAS_Main.py
+++ UAS_Main.py
@@ -7,16 +7,35 @@ import RPi.GPIO as GPIO
 from pixy import *
 from ctypes import *
 
+#connect to PI through serial
+vehicle = connect(/dev/ttyAMA0, wait_ready=True)
+
+print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+while not vehicle.is_armable:
+    print(" Waiting for vehicle to initialise...")
+    time.sleep(1)
+
+
+print("Arming motors")
+# Copter should arm in GUIDED mode
+vehicle.mode = VehicleMode("GUIDED")
+vehicle.armed = True
+    
+while not vehicle.armed:
+    print(" Waiting for arming...")
+    time.sleep(1)
+
 # Initialize Pixy Interpreter thread #
 pixy_init()
 global Xaxis, Yaxis
 
-Centering()
+#Centering()
 
 
 '''
 import argparse
-parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser = argparse.ArgumentParser(description='Start up command for Vehicle')
 parser.add_argument('--connect',
                     help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
